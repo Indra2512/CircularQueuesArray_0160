fd1adab50b73af6a9e20590ced4e7e235fa3a175
Indra2512 <mononeon1225@gmail.com>
2025-06-12 10:22:31 +0700

    menambahkan fungsi remove

--- Queue.cpp
+++ Queue.cpp
@@ -39,6 +39,21 @@ class Queues
             {
                 //jika REAR berada di posisi terakhir array, kembali ke awal array
                 if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    REAR = REAR + 1;
+            }
+            queue_array[REAR] = num;
+        }
+
+        void remove()
+        {
+            //cek apakah antrian kosong
+            if (FRONT == -1)
+            {
+                cout << "Queue underflow\n";
+                return;
             }
         }
+
 };
