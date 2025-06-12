d1a1082a380ff75bd68a8821662088f17f1e45d1
Indra2512 <mononeon1225@gmail.com>
2025-06-12 10:53:50 +0700

    Enter your choice (1-4)

--- Queue.cpp
+++ Queue.cpp
@@ -123,6 +123,7 @@ int main()
             cout << "2. Implement delete peration" << endl;
             cout << "3. Display values" << endl;
             cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4): ";
         }
     
     }
