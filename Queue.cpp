b6b09b8ece4a023f2054332cf512bed9e6aa94c1
Indra2512 <mononeon1225@gmail.com>
2025-06-12 10:31:39 +0700

    jika element yang di hapus berada di posisi terakhir, kembali ke awal array

--- Queue.cpp
+++ Queue.cpp
@@ -64,8 +64,13 @@ class Queues
             }
             else
             {
-
+                //jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+                if (FRONT == max -1)
+                    FRONT = 0;
+                else
+                    FRONT = FRONT + 1;
             }
         }
+
 
 };
